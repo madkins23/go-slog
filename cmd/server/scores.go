dd20f34be38d04e03e699e9c76f5d9772507c017
mAdkins <software@doorways.org>
2024-03-22 16:29:36 -0700

    Comment cmd/server/scores.go

--- cmd/server/scores.go
+++ cmd/server/scores.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"math"
 	"net/http"
 	"sort"
 	"strconv"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/wcharczuk/go-chart/v2"
@@ -19,7 +17,8 @@ import (
 
 // -----------------------------------------------------------------------------
 
-// scoreFunction generates an SVG chart for the score visualization.
+// scoreFunction generates an SVG chart for the score visualization and
+// uses the gin.Context argument to send the SVG data back to the user's browser.
 func scoreFunction(c *gin.Context) {
 	cacheKey := "score"
 	chartCacheMutex.Lock()
@@ -43,10 +42,15 @@ func scoreFunction(c *gin.Context) {
 // -----------------------------------------------------------------------------
 
 const (
+	// height of chart in pixels
 	height = 600
-	width  = 750
+
+	// width of chart in pixes
+	width = 750
 )
 
+// scoreChart generates a chart.Chart object which is a scatter plot of
+// handler benchmark vs. warning scores.
 func scoreChart() chart.Chart {
 	type handlerCoords struct{ x, y float64 }
 	handlers := make(map[data.HandlerTag]*handlerCoords)
@@ -115,29 +119,22 @@ func scoreChart() chart.Chart {
 
 // -----------------------------------------------------------------------------
 
+// scoreChartAdjustLabels
 func scoreChartAdjustLabels(locations []chart.Value2) []chart.Value2 {
 	groups := make([]*labelGroup, 0, 10)
 	singles := make([]int, 0, len(locations))
 
-	fmt.Println(">>> scoreChartAdjustLabels()")
-	for i, loc := range locations {
-		fmt.Printf(">>>   [%d] %s\n", i, loc.Label)
-	}
-
-	fmt.Printf(">>> First iteration\n")
-location:
+	// Pull out groups and collect remaining singles.
+location: // OMG I can't believe he's using that named loop thingy!!!
 	for i := 0; i < len(locations); i++ {
-		fmt.Printf(">>>  [%d] %s\n", i, locations[i].Label)
 		loc := locations[i]
 		for _, group := range groups {
 			if group.contains(i) {
-				fmt.Printf(">>>   group contains %d\n", i)
 				continue location
 			}
 		}
 		for _, group := range groups {
 			if group.overlaps(loc) {
-				fmt.Printf(">>>   group overlaps %d\n", i)
 				group.add(i, loc)
 				continue location
 			}
@@ -145,7 +142,6 @@ location:
 		for j := i + 1; j < len(locations); j++ {
 			other := locations[j]
 			if math.Abs(other.XValue-loc.XValue) < labelWidth && math.Abs(other.YValue-loc.YValue) < labelHeight {
-				fmt.Printf(">>>   group from %d & %d\n", i, j)
 				group := newLabelGroup()
 				group.add(i, loc)
 				group.add(j, other)
@@ -155,18 +151,21 @@ location:
 		}
 		singles = append(singles, i)
 	}
-	fmt.Printf(">>> Second iteration\n")
+
+	// Check the remaining singles against the existing groups.
 	for _, index := range singles {
 		for _, group := range groups {
 			if group.overlaps(locations[index]) {
-				fmt.Printf(">>>  group overlaps %d\n", index)
 				group.add(index, locations[index])
 			}
 		}
 	}
+	// Adjust label locations in each group.
+	// Singles don't need any adjustment.
 	for _, group := range groups {
 		group.adjust(locations)
 	}
+	// Return for convenience, likely already edited in place.
 	return locations
 }
 
@@ -174,6 +173,9 @@ location:
 
 const bigByte = float64(0xFF)
 
+// scoreChartColorFunction returns a color dependent on the chart location.
+// The algorithm is intended to make it red in the lower left and green in the upper right
+// with gradual color changes in between.
 func scoreChartColorFunction(xr, yr chart.Range, _ int, x, y float64) drawing.Color {
 	ratio := scoreChartRatio(xr.GetMin(), yr.GetMin(), xr.GetMax(), yr.GetMax())
 	diagonal := scoreChartDistance(xr.GetMin(), yr.GetMin(), xr.GetMax(), yr.GetMax(), ratio)
@@ -187,10 +189,12 @@ func scoreChartColorFunction(xr, yr chart.Range, _ int, x, y float64) drawing.Co
 	}
 }
 
+// scoreChartRatio is a convenience function for use in scoreChartColorFunction.
 func scoreChartRatio(xMin, yMin, xMax, yMax float64) float64 {
 	return (xMax - xMin) / (yMax - yMin)
 }
 
+// scoreChartColor is a convenience function for use in scoreChartColorFunction.
 func scoreChartColor(distance, diagonal float64) uint8 {
 	if distance > diagonal {
 		return 0
@@ -198,19 +202,29 @@ func scoreChartColor(distance, diagonal float64) uint8 {
 	return byte(bigByte - bigByte*distance/diagonal)
 }
 
+// scoreChartDistance is a convenience function for use in scoreChartColorFunction.
 func scoreChartDistance(xMin, yMin, xMax, yMax, ratio float64) float64 {
 	return math.Sqrt(math.Pow((xMax-xMin)/ratio, 2) + math.Pow(yMax-yMin, 2))
 }
 
 // -----------------------------------------------------------------------------
 
+// labelGroup collects labels that are close together into a group.
 type labelGroup struct {
-	indexMap    map[int]bool
-	indices     []int
+	// "Set" of index integers in the group for quick lookup.
+	indexMap map[int]bool
+
+	// "List" of index integers in the group for array access.
+	indices []int
+
+	// High and low x-coordinate values for all labels in the group.
 	xHigh, xLow float64
+
+	// High and low y-coordinate values for all labels in the group.
 	yHigh, yLow float64
 }
 
+// newLabelGroup returns a new labelGroup.
 func newLabelGroup() *labelGroup {
 	return &labelGroup{
 		indexMap: make(map[int]bool),
@@ -222,6 +236,7 @@ func newLabelGroup() *labelGroup {
 	}
 }
 
+// add a label index and chart value to the group.
 func (lg *labelGroup) add(index int, loc chart.Value2) {
 	if _, found := lg.indexMap[index]; !found {
 		lg.indexMap[index] = true
@@ -240,17 +255,21 @@ func (lg *labelGroup) add(index int, loc chart.Value2) {
 }
 
 const (
-	labelWidth  = 25.0
+	// Approximate width of a label along the x-axis in percentage coordinates (not pixels).
+	// This is a trial-and-error value :-(.
+	labelWidth = 25.0
+
+	// Approximate height of a label along the y-axis in percentage coordinates (not pixels).
+	// This is a trial-and-error value :-(.
 	labelHeight = 4.5
 )
 
+// adjust labels in the group by editing their y-coordinate values until they don't overlap.
 func (lg *labelGroup) adjust(locations []chart.Value2) {
 	labels := make([]string, 0, len(lg.indices))
 	for _, index := range lg.indices {
 		labels = append(labels, locations[index].Label)
 	}
-	fmt.Printf(">>> adjust(%d) %s\n", len(lg.indices), strings.Join(labels, ", "))
-
 	if len(locations) < 2 {
 		return
 	}
@@ -261,6 +280,7 @@ func (lg *labelGroup) adjust(locations []chart.Value2) {
 		return locations[lg.indices[i]].YValue <= locations[lg.indices[j]].YValue
 	})
 
+	// Initialization of various state variables.
 	var upDebt, downDebt float64
 	var upIndex, downIndex int
 	var center int
@@ -283,8 +303,8 @@ func (lg *labelGroup) adjust(locations []chart.Value2) {
 		upIndex = center
 	}
 
+	// Adjust labels in the negative y direction.
 	for downIndex > 0 {
-		fmt.Printf(">>> downIndex: %d\n", downIndex)
 		overlap := labelHeight - (locations[lg.indices[downIndex]].YValue - locations[lg.indices[downIndex-1]].YValue)
 		if overlap > 0 {
 			downDebt += overlap
@@ -293,8 +313,8 @@ func (lg *labelGroup) adjust(locations []chart.Value2) {
 		downIndex--
 	}
 
+	// Adjust labels in the positive y direction.
 	for upIndex < len(lg.indices)-1 {
-		fmt.Printf(">>> upIndex: %d\n", downIndex)
 		overlap := labelHeight - (locations[lg.indices[upIndex+1]].YValue - locations[lg.indices[upIndex]].YValue)
 		if overlap > 0 {
 			upDebt += overlap
@@ -304,11 +324,13 @@ func (lg *labelGroup) adjust(locations []chart.Value2) {
 	}
 }
 
+// contains returns true if the specified index is part of the group.
 func (lg *labelGroup) contains(index int) bool {
 	_, found := lg.indexMap[index]
 	return found
 }
 
+// overlaps returns true if the specified chart value overlaps the group.
 func (lg *labelGroup) overlaps(loc chart.Value2) bool {
 	return loc.XValue >= lg.xLow-labelWidth && loc.XValue <= lg.xHigh+labelWidth &&
 		loc.YValue >= lg.yLow-labelHeight && loc.YValue <= lg.xLow+labelHeight
