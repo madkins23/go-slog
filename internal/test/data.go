350fa758ee45a89eb856fc1339c05bb543f1be14
mAdkins <software@doorways.org>
2024-04-28 12:25:01 -0700

    Update documentation for internal/test

--- internal/test/data.go
+++ internal/test/data.go
@@ -68,3 +68,12 @@ var (
 		},
 	}
 )
+
+var EscapeCases = map[string]string{
+	"3": `3`,
+	"The quick brown fox jumped over the lazy dog.":    `The quick brown fox jumped over the lazy dog.`,
+	"Control characters:  \b, \f, \n, \r, \t":          `Control characters:  \b, \f, \n, \r, \t`,
+	"Quote and slashes: \", \\, /":                     `Quote and slashes: \", \\, \/`,
+	"UTF8 Characters: ϢӦֆĒ͖̈́Ͳ  ĦĪǂǼɆψϠѬӜԪ":             `UTF8 Characters: ϢӦֆĒ͖̈́Ͳ  ĦĪǂǼɆψϠѬӜԪ`,
+	"Unicode Characters: 😀🐦‍🔥⛓️‍💥🍋‍🟩  ظۇ  ❂✈☯  亳亴亵亶亷亸": `Unicode Characters: 😀🐦‍🔥⛓️‍💥🍋‍🟩  ظۇ  ❂✈☯  亳亴亵亶亷亸`,
+}
--- internal/test/doc.go
+++ internal/test/doc.go
@@ -1,5 +1,7 @@
 // Package test provides some utilities for construct test files.
-// * Debugf provides a simplistic logging for use in test cases.
-// * Case defines test cases stored in JSON files.
-// * Manager for use collecting and displaying warnings.
+//
+//   - Case defines test cases stored in JSON files.
+//   - CountWriter is an io.Writer that counts lines and bytes and then throws them away.
+//   - Debugf provides a simplistic logging for use in test cases.
+//   - Various constants and variables for use in multiple testing files.
 package test
