d3a64d25aff9065300a205be15bb8dd4ecbe5550
mAdkins <software@doorways.org>
2024-05-15 16:28:43 -0700

    Exhibit tables now showing up

--- internal/scoring/score/exhibit.go
+++ internal/scoring/score/exhibit.go
@@ -8,25 +8,21 @@ import (
 	"github.com/gomarkdown/markdown/html"
 )
 
-type Exhibit struct {
-	name string
+type Exhibit interface {
+	HTML(data any) template.HTML
+}
+
+type ExhibitCore struct {
 	tmpl *template.Template
 }
 
-func NewExhibit(name string, tmpl *template.Template) Exhibit {
-	return Exhibit{
-		name: name,
-		tmpl: tmpl,
-	}
+func NewExhibitCore(tmpl *template.Template) ExhibitCore {
+	return ExhibitCore{tmpl: tmpl}
 }
 
-func (e *Exhibit) Name() string {
-	return e.name
-}
-
-func (e *Exhibit) HTML(data any) template.HTML {
+func (ec *ExhibitCore) HTML(data any) template.HTML {
 	var buf bytes.Buffer
-	if err := e.tmpl.Execute(&buf, data); err != nil {
+	if err := ec.tmpl.Execute(&buf, data); err != nil {
 		var errorText bytes.Buffer
 		html.EscapeHTML(&errorText, []byte(err.Error()))
 		// TODO: Make something more general, read markdown from a file or something?
