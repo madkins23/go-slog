d52dcd8ccd56a6bf8acb2d3365da03042e3c07a4
Marc M. Adkins <software@doorways.org>
2024-08-11 15:19:52 -0700

    Finish reverting veqryn/dedup function names to match actual mode names in doc

--- bench/veqryn_dedup_test.go
+++ bench/veqryn_dedup_test.go
@@ -7,14 +7,14 @@ import (
 	"github.com/madkins23/go-slog/creator/veqryndedup"
 )
 
-// BenchmarkVeqrynDedupGroup runs benchmarks for the veqryn/dedup JSON handler in Append mode.
-func BenchmarkVeqrynDedupGroup(b *testing.B) {
+// BenchmarkVeqrynDedupAppend runs benchmarks for the veqryn/dedup JSON handler in Append mode.
+func BenchmarkVeqrynDedupAppend(b *testing.B) {
 	slogSuite := tests.NewSlogBenchmarkSuite(veqryndedup.Creator(veqryndedup.Append))
 	tests.Run(b, slogSuite)
 }
 
-// BenchmarkVeqrynDedupFirst runs benchmarks for the veqryn/dedup JSON handler in Ignore mode.
-func BenchmarkVeqrynDedupFirst(b *testing.B) {
+// BenchmarkVeqrynDedupIgnore runs benchmarks for the veqryn/dedup JSON handler in Ignore mode.
+func BenchmarkVeqrynDedupIgnore(b *testing.B) {
 	slogSuite := tests.NewSlogBenchmarkSuite(veqryndedup.Creator(veqryndedup.Ignore))
 	tests.Run(b, slogSuite)
 }
@@ -25,8 +25,8 @@ func BenchmarkVeqrynDedupIncrement(b *testing.B) {
 	tests.Run(b, slogSuite)
 }
 
-// BenchmarkVeqrynDedupLast runs benchmarks for the veqryn/dedup JSON handler in Overwrite mode.
-func BenchmarkVeqrynDedupLast(b *testing.B) {
+// BenchmarkVeqrynDedupOverwrite runs benchmarks for the veqryn/dedup JSON handler in Overwrite mode.
+func BenchmarkVeqrynDedupOverwrite(b *testing.B) {
 	slogSuite := tests.NewSlogBenchmarkSuite(veqryndedup.Creator(veqryndedup.Overwrite))
 	tests.Run(b, slogSuite)
 }
